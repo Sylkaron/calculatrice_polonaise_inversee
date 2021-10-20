6657ef995450062b3803960d13236e54537a4f71
Sylkaron <pabloberton@sfr.fr>
2021-10-20 13:54:17 +0200

    Start

---
+++ Calculatrice_polonaise_inversee/src/com/pablo/Accumulateur.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package com.pablo;
+
+/**
+ * @author pablo
+ *
+ */
+public class Accumulateur implements IAccumulateur {
+	
+	//Attributes
+	
+	
+
+	/**
+	 * 
+	 */
+	public Accumulateur() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void swap() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mult() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void div() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void neg() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void backspace() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void accumuler(char caractere) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
